7c8f9729676d549716eb55a229d9f08a515ec8aa
Chris <chris@linux-1anr.site>
2015-11-10 12:23:04 +0100

    hi alina

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,75 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+complex<double>comptan(double phi);
 
 int main(){
+  
+  ofstream out("output.dat");
+  ofstream outb("outputb.dat");
+  
 
   // Example how to declare a single complex variable:
   complex<double> c;
 
   // d = 1 + 2i 
   complex<double> d = complex<double>(1.0, 2.0); 
+  
+  int N = 100;
+  
+  double data[N][4];
+  double datab[N][4];
+  double phi=0;
+  double phib = 0;
+  complex<double> z= complex<double>(0.0, 0.0);
+  complex<double> zb= complex<double>(0.0, 0.0);
+  
+  //initialize phi_i
+  for (int i=0; i<N; i++){
+    //compute data
+    phi = (2*M_PI/N) *i;
+    phib = phi - M_PI;
+    z = complex<double>(cos(phi), sin(phi));
+    zb = comptan(phib);
+    
+    //save data
+    data[i][0] = phi;
+    data[i][1] = real(z);
+    data[i][2] = imag(z);
+    data[i][3] = abs(z);
+    
+    datab[i][0] = phib;
+    datab[i][1] = real(zb);
+    datab[i][2] = imag(zb);
+    datab[i][3] = abs(zb);
+  }
+  
+  //write data to file
+  for (int i=0; i<N; i++){
+    out << data[i][0] << "\t" << data[i][1] << "\t" << 
+    data[i][2] << "\t" << data[i][3] << endl;
+  }
+  
+  
+  for (int i=0; i<N; i++){
+    outb << datab[i][0] << "\t" << datab[i][1] << "\t" << 
+    datab[i][2] << "\t" << datab[i][3] << endl;
+  }
+  
 
+  out.close();
+  outb.close();
+  
   return 0;
+}
+
+complex<double> comptan(double phi){
+  complex<double> z;
+  complex<double> ii = complex<double>(0.0,1.0);
+  z = tan(phi*(1.0+ii));
+  return z;
 }
--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <complex>
 
+using namespace std;
+
 int main(){
 
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  
+  int Nmax= 400;
+  double tol = 10E-8;
+  double err = 1;
+  
+  int i;
+  complex<double> zlimit;
+  complex<double> z0;
+  
+  for (double re=-2; re<=2; re+= 0.01){
+    for (double im= -2; im<= 2; im+=0.01){
+      
+      z0 = complex<double>(re, im);
+      err= 1;
+      i = 0;
+      while(i <= Nmax && err >= tol){
+	zlimit = z0 - (pow(z0,3)-1.0)/(3.0*pow(z0,2));
+	
+	err = abs(zlimit- z0);
+	z0 = zlimit;
+	i++;
+      }
+      //print current values to consol
+      cout << re << "\t" << im << "\t" << i << endl;
+    }
+  }
 
   return 0;
 }
